c203120b7cf5bf1fe739215fdc941d3088867cfd
thebudjr <tim.s.janney@gmail.com>
2015-07-29 19:54:45 -0400

    added user sorting on people index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
 
 	before_action :set_user, only: :show
+  before_action :authenticate_user!, only: [:index]
 
 	def index
-		@users = User.all
+    case params[:people]
+      when "friends"
+        @users = current_user.active_friends
+      when "requests"
+        @users = current_user.pending_friend_requests_from.map(&:user)
+      when "pending"
+        @users = current_user.pending_friend_requests_to.map(&:user)
+      else
+        @users = User.where.not(id: current_user.id)
+    end
 	end
 
   def show
